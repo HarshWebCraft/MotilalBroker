5e42b8a28165bdc27adbe78ab80abc9ea51e0ac6
Harsh WebCraft <harshwebcraft@gmail.com>
2026-05-30 11:44:57 +0530

    a

--- searchSymbol.js
+++ searchSymbol.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const DATA_DIR = path.join(__dirname, "../data");
+const DATA_DIR = path.join(__dirname, "./data");
 const ALLOWED_EXCHANGES = ["NSE", "BSE", "NSEFO", "BSEFO", "MCX", "CDS"];
 function parseExpiry(expiry) {
   if (!expiry) return null;
